3f685f6ebc1374aa761343a80ce3ad20d9422544
Angel <soporte@libus.com>
2022-09-12 16:59:21 -0300

    Se inicia pagina de prueba para next JS

---
+++ pages/faq.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+export default function FAQ(){
+    return (
+        <div>
+            <h1>FAQ</h1>
+            <p>Hola{" "}
+            <Link href='/'>
+                <a>Ir a Home</a>
+            </Link>
+            </p>
+        </div>
+    )
+}
---
+++ pages/store/index.js
@@ -0,0 +1,7 @@
+export default function store(){
+    return (
+    <div>
+        <h1>Bienvenidos al Store</h1>
+    </div>
+    )
+}
